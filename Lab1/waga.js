8095b98653bfa2f45cc6e8f1c03e6c234ab8454e
mgorczynsky2 <115210575+mgorczynsky2@users.noreply.github.com>
2024-10-14 17:27:20 +0200

    lab2 finished

--- Lab1/waga.js
+++ Lab1/waga.js
@@ -4,18 +4,18 @@ function calculateWeight() {
 	let weightOnPlanet;
 	
 	const gravity = {
-		Mercury: 0.38,
+		Merkury: 0.38,
 		Venus: 0.91,
 		Mars: 0.38,
-		Jupiter: 2.34,
+		Jowisz: 2.34,
 		Saturn: 1.06,
-		Uranus: 0.92,
-		Neptune: 1.19
+		Uran: 0.92,
+		Neptun: 1.19
 		};
 	if (weight && gravity[planet]){
 		weightOnPlanet=weight*gravity[planet];
 		
-		document.getElementById('result').innerText = `Na plenecie wazysz ${weightOnPlanet.toFixed(2)} kg`;
+		document.getElementById('result').innerText = `Na plenecie ${planet} wazysz ${weightOnPlanet.toFixed(2)} kg`;
 		}
 		else{
 		document.getElementById('result').innerText=`Wprowadz prawidlowe dane`;
